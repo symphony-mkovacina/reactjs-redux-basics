407fe96c64b4a2bdf81bdeb56ae00f7138655c55
Marko Kovacina <marko.kovacina@symphony.is>
2017-08-27 01:53:53 +0200

    Initial commit.

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,67 @@
-import React from "react";
-import {render} from "react-dom";
+// import React from "react";
+// import {render} from "react-dom";
+//
+// import { User } from './components/User';
+// import { Main } from './components/Main';
+//
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: "Max"
+//         };
+//     }
+//
+//     changeUsername(newName) {
+//         this.setState({
+//             username: newName
+//         });
+//     }
+//
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this.changeUsername.bind(this)}/>
+//                 <User username={this.state.username}/>
+//             </div>
+//         );
+//     }
+// }
+//
+// render(<App />, window.document.getElementById('app'));
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+import {createStore} from "redux";
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
-        };
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = state + action.payload;
+            break;
+        case "SUBTRACT":
+            state = state - action.payload;
+            break;
     }
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
+    return state;
+};
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
-}
+const store = createStore(reducer, 1);
 
-render(<App />, window.document.getElementById('app'));
+store.subscribe(() => {
+    console.log("Store updated!", store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 22
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 80
+});
