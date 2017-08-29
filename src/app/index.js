a5dbd749d35056b367d3067f4c3f5acfb61db579
Marko Kovacina <marko.kovacina@symphony.is>
2017-08-30 00:11:35 +0200

    Containers & Components (Smart & Dumb Components)

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import {createStore, combineReducers, applyMiddleware} from "redux";
 import {createLogger} from "redux-logger";
 import {Provider} from "react-redux";
 
-import App from "./components/App";
+import App from "./containers/App";
 
 const mathReducer = (state = {
     result: 1,
